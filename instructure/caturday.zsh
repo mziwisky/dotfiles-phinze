3d511b3374897b500fcf90b0a597878b99003f1f
phinze <paul.t.hinze@gmail.com>
2013-03-15 16:17:25 -0500

    remove powerline, add syntastic and tabular

---
+++ instructure/caturday.zsh
@@ -0,0 +1,6 @@
+export DEPLOY_BUNDLE_19=/Users/paulh/.rbenv/versions/1.9.3-p327/bin/bundle
+export DEPLOY_BUNDLE_18=/Users/paulh/.rbenv/versions/ree-1.8.7-2012.02/bin/bundle
+
+export CATURDAY_SERVICE_USER="caturday"
+export CATURDAY_SERVICE_PASSWORD="$(security find-generic-password -s 'caturday_service' -w)"
+export CATURDAY_SERVICE_URL="$(security find-generic-password -s 'caturday_service' | grep acct | cut -d= -f2 | sed 's/"//g')"
